f30939cb5dc94667b9bbeaf9edf84e85efe012ea
Vicente Rodriguez <dev.vrodriguez@gmail.com>
2019-08-18 15:51:06 -0400

    arrows

--- components/lesson/lessonComponents.js
+++ components/lesson/lessonComponents.js
@@ -5,67 +5,57 @@ import {
   Button,
   Reveal,
   Icon,
-  Header,
-  Segment
+  Card,
+  Image
 } from 'semantic-ui-react';
 
 const getNavigationParams = (direction) => {
   // Right by default
-  let dirTextStr = 'Next Lesson';
-  let labelPosStr = 'right';
-  let iconClsStr = 'angle right';
-  let padding = { left: '2px', right: '0px' };
+  let dirText = 'Next Lesson';
+  let labelPos = 'right';
+  let icon = 'angle right';
+  let margin = { 'margin-left': '2em' };
   let moveDir = 'move right';
   if (direction === 'left') {
-    dirTextStr = 'Previous Lesson';
-    labelPosStr = 'left';
-    iconClsStr = 'angle left';
-    padding = { left: '0px', right: '2px' };
+    dirText = 'Previous Lesson';
+    labelPos = 'left';
+    icon = 'angle left';
+    margin = { 'margin-left': '2em' };
     moveDir = 'move';
   }
-  return {
-    dirText: dirTextStr,
-    labelPos: labelPosStr,
-    iconCls: iconClsStr,
-    pad: padding,
-    move: moveDir
-  };
+  const res = {};
+  res.dirText = dirText;
+  res.labelPos = labelPos;
+  res.icon = icon;
+  res.margin = margin;
+  res.moveDir = moveDir;
+  return res;
 };
 
-export const NavigationComp = ({ direction }) => {
-  const dirParams = getNavigationParams(direction);
-  return (
-    <Button
-      fluid
-      color="red"
-      content={dirParams.dirText}
-      icon={dirParams.iconCls}
-      labelPosition={dirParams.labelPos}
-      verticalAlign={dirParams.labelPos}
-      style={{ 'padding-top': '2em', 'padding-bottom': '2em' }}
-    />
-  );
-};
+export const LessonNavigationButton = ({ dirParams }) => (
+  <Button
+    fluid
+    color="red"
+    content={dirParams.dirText}
+    icon={dirParams.icon}
+    labelPosition={dirParams.labelPos}
+    style={{ paddingTop: '1.8em', paddingBottom: '1.8em' }}
+  />
+);
 
-export const NavigationCompWithReveal = ({ direction }) => {
+export const LessonNavReveal = ({ direction }) => {
   const dirParams = getNavigationParams(direction);
   return (
-    <Reveal animated={dirParams.move} instant>
-      <Reveal.Content as="Segment" visible>
-        <div style={{}}>
-          <Icon name={dirParams.iconCls} size="huge" />
-        </div>
+    <Reveal animated={dirParams.moveDir} style={{ 'border-radius': '5px' }}>
+      <Reveal.Content visible>
+        <Card>
+          <Image fluid centered style={{ 'background-color': '#1b1c1d!important' }}>
+            <Icon fluid inverted bordered name={dirParams.icon} size="big" style={dirParams.margin} />
+          </Image>
+        </Card>
       </Reveal.Content>
-      <Reveal.Content as="Segment" hidden>
-        <Button
-          fluid
-          color="red"
-          content={dirParams.dirText}
-          icon={dirParams.iconCls}
-          labelPosition={dirParams.labelPos}
-          verticalAlign={dirParams.labelPos}
-          style={{ 'padding-top': '2em', 'padding-bottom': '2em' }}
-        />
+      <Reveal.Content hidden>
+        <LessonNavigationButton dirParams={dirParams} />
       </Reveal.Content>
     </Reveal>
   );
--- pages/lessons.js
+++ pages/lessons.js
@@ -11,58 +11,37 @@ import {
   Embed,
   Form,
   TextArea,
-  Grid,
-  Reveal,
-  Icon,
-  Button
+  Grid
 } from 'semantic-ui-react';
 import ResponsiveContainer from '../components/containers/ResponsiveContainer';
-import { NavigationComp, NavigationCompWithReveal } from '../components/lesson/lessonComponents';
+import { LessonNavReveal } from '../components/lesson/lessonComponents';
 
 const lesson1 = {
   title: 'Mrtoefl Lesson One',
   description: 'In this lesson you will learn the basics'
 };
 
-const NavReveal = ({ direction }) => {
-  const move = direction === 'left' ? 'move' : 'move right';
-  return (
-    <Reveal animated={move} instant>
-      <Reveal.Content as="Segment" visible>
-        <Container fluid textAlign="center">
-          <Icon inverted bordered color="white" name="angle right" size="big" />
-        </Container>
-      </Reveal.Content>
-      <Reveal.Content as="Segment" hidden>
-        <NavigationComp direction={direction} />
-      </Reveal.Content>
-    </Reveal>
-  );
-};
-
 const Lesson = ({ lessonContent }) => (
   <Container>
     <Segment vertical>
-      <div>
-        <Segment vertical padded>
-          <Grid container columns={3}>
-            <Grid.Row stretched>
-              <Grid.Column width={3} stretched style={{ 'padding-right': '2px' }}>
-                <NavReveal direction="left" />
-              </Grid.Column>
-              <Grid.Column width={10} inverted color="red" verticalAlign="middle" style={{ 'border-radius': '5px' }}>
-                <Header inverted as="h2">
-                  <b>{lessonContent.title}</b>
-                  <Header.Subheader>{lessonContent.description}</Header.Subheader>
-                </Header>
-              </Grid.Column>
-              <Grid.Column width={3} stretched style={{ 'padding-right': '0px', 'padding-left': '2px' }}>
-                <NavReveal direction="right" />
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
-        </Segment>
-      </div>
+      <Segment vertical padded>
+        <Grid container columns={3}>
+          <Grid.Row stretched>
+            <Grid.Column width={3} stretched>
+              <LessonNavReveal direction="left" />
+            </Grid.Column>
+            <Grid.Column width={10} inverted color="red" verticalAlign="middle" style={{ 'border-radius': '5px' }}>
+              <Header inverted as="h2">
+                <b>{lessonContent.title}</b>
+                <Header.Subheader>{lessonContent.description}</Header.Subheader>
+              </Header>
+            </Grid.Column>
+            <Grid.Column width={3} stretched>
+              <LessonNavReveal direction="right" />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
       <Container text>
         <Segment secondary padded>
           <Header as="h3">Lesson Content</Header>
@@ -78,19 +57,6 @@ const Lesson = ({ lessonContent }) => (
           </ol>
         </Segment>
       </Container>
-    </Segment>
-    <Segment vertical padded>
-      <Header as="h2">
-        <b>Lesson 1:</b> Video Example
-        <Header.Subheader>Pay attention to the video and take notes if you want</Header.Subheader>
-      </Header>
-      <Embed id="188171839" placeholder="../static/img/logo-med.png" source="vimeo" defaultActive="true" />
-    </Segment>
-    <Segment vertical>
-      <Header as="h2">Notes</Header>
-      <Form>
-        <TextArea placeholder="Take your notes here" style={{ minHeight: 150 }} />
-      </Form>
     </Segment>
   </Container>
 );
